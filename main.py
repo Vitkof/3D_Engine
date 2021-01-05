5f8825ec3473dd1694d7555f6a4b3e40798c53dc
Victor <71389732+Vitkof@users.noreply.github.com>
2021-01-05 13:38:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ class SoftwareRender:
         self.RESOLUTION = self.WIDTH, self.HEIGHT = 800, 600
         self.FPS = 60
         self.window = pg.display.set_mode(self.RESOLUTION, pg.RESIZABLE)
-        self.W, self.H = 500, 400
+        self.W_2, self.H_2 = self.WIDTH // 2, self.HEIGHT // 2
         self.timer = pg.time.Clock()
         self.create_obj()
         #self.object = Object3D(self) без метода, тоже самое
 
     def draw(self):
         self.window.fill(color='darkgray')
+        self.object.draw()
 
         for e in pg.event.get():
             if e.type == pg.QUIT:
@@ -25,7 +26,10 @@ class SoftwareRender:
         self.timer.tick(self.FPS)
 
     def create_obj(self):
+        self.camera = Camera(self, [0.5, 0.9, -2.5])
+        self.projection = Projection(self)
         self.object = Object3D(self)
+        self.object.rotate_Z(math.pi / 6)
 
 
 if __name__ == "__main__":
