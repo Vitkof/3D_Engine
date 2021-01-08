e2649ce2b12a75986d4edd2bb9c779df6321fabd
Victor <71389732+Vitkof@users.noreply.github.com>
2021-01-08 14:28:36 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -26,6 +26,50 @@ class Camera:
         # Ближняя и Дальняя области плоскости усеченной пирамиды обзора
         self.near_plane = 0.1
         self.far_plane = 100
+        # Скорость движения/вращения
+        self.moving_speed = 0.02
+        self.rotation_speed = 0.5   # в градусах
+
+    def controller(self):
+        kl = pg.key.get_pressed()
+        if kl[pg.K_a]:
+            self.pos += self.right * self.moving_speed
+        if kl[pg.K_d]:
+            self.pos -= self.right * self.moving_speed
+        if kl[pg.K_w]:
+            self.pos += self.forward * self.moving_speed
+        if kl[pg.K_s]:
+            self.pos -= self.forward * self.moving_speed
+        if kl[pg.K_q]:
+            self.pos -= self.up * self.moving_speed
+        if kl[pg.K_e]:
+            self.pos += self.up * self.moving_speed
+        if kl[pg.K_LEFT]:
+            self.camera_yaw(math.pi / 180 * self.rotation_speed)
+        if kl[pg.K_RIGHT]:
+            self.camera_yaw(-math.pi / 180 * self.rotation_speed)
+        if kl[pg.K_UP]:
+            self.camera_pitch(math.pi / 180 * self.rotation_speed)
+        if kl[pg.K_DOWN]:
+            self.camera_pitch(-math.pi / 180 * self.rotation_speed)
+
+    def camera_pitch(self, angle):
+        rtt = rotation_X(angle)
+        self.right = self.right @ rtt
+        self.up = self.up @ rtt
+        self.forward = self.forward @ rtt
+
+    def camera_yaw(self, angle):
+        rtt = rotation_Y(angle)
+        self.right = self.right @ rtt
+        self.up = self.up @ rtt
+        self.forward = self.forward @ rtt
+        
+    def camera_roll(self, angle):
+        rtt = rotation_Z(angle)
+        self.right = self.right @ rtt
+        self.up = self.up @ rtt
+        self.forward = self.forward @ rtt
 
     def translate_matrix(self):
         x, y, z, w = self.pos
